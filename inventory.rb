5f1501769618ba80348659f919a1491b537a860d
Caleb Lee <caleb.lee@vokalinteractive.com>
2015-06-17 16:33:06 -0500

    Adds methods to create formatted JSON strings from data in Inventory

--- inventory.rb
+++ inventory.rb
@@ -5,12 +5,13 @@ class Inventory
     @products = Array.new
   end
   
-  def initialize(numberOfProducts)
-    @products = Array.new(numberOfProducts)
-  end
-  
   def fullInventory
     return @products
+  end
+  
+  # formats the inventory into a json string for a server response
+  def fullFormattedInventory
+    formattedProductStringFromArray(@products)
   end
   
   # returns nil if there isn't a product with that name
@@ -24,7 +25,40 @@ class Inventory
     return nil
   end
   
+  # finds the single product and formats it into a JSON string
+  # returns nil if the product isn't found
+  def formattedStringForProductWithName(name)
+    product = productWithName(name)
+    arrayWithProduct = [product]
+    
+    return formattedProductStringFromArray(arrayWithProduct)
+  end
+  
   def addProduct(product)
-    @products.push(product)
+    @products[@products.count] = product
+  end
+  
+  private 
+  
+  # helper method to keep all the string formatting code in one place
+  def formattedProductStringFromArray(arrayOfProducts)
+    result = "{\n"
+	
+	index = 0
+	for product in arrayOfProducts
+	  result = result + "  \"#{product.name}\": #{product.quantity}"
+	  
+	  # don't add the comma on the last line
+	  index += 1
+	  if arrayOfProducts.count == index
+	    result = result + "\n"
+	  else
+	    result = result + ",\n"
+	  end
+	end
+	
+	result = result + "}"
+	
+    return result
   end
 end
