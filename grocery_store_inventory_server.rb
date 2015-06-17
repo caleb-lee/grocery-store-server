df0e7e77e3a25a4522b27fc17d623a25378fd331
Caleb Lee <caleb.lee@vokalinteractive.com>
2015-06-17 16:38:00 -0500

    Server response gives formatted inventory from /api/inventory

--- grocery_store_inventory_server.rb
+++ grocery_store_inventory_server.rb
@@ -5,9 +5,11 @@ require './inventory'
 
 # set up an inventory to do work on
 products = Inventory.new
-
-apple = Product.new("apples", 123)
-products.addProduct(apple)
+products.addProduct(Product.new("apples", 123))
 products.addProduct(Product.new("oranges", 62))
 products.addProduct(Product.new("milk", 54))
 products.addProduct(Product.new("eggs", 22))
+
+get '/api/inventory' do
+  products.fullFormattedInventory
+end
