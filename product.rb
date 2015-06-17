5d26915b15b153bb21c86cc60b654edec366ccd9
Caleb Lee <caleb.lee@vokalinteractive.com>
2015-06-17 15:41:09 -0500

    Adds function to product to create a hash of the product

--- product.rb
+++ product.rb
@@ -1,4 +1,6 @@
 class Product
+  # name -> string
+  # quantity -> int
   def initialize(name, quantity)
   	@name = name
   	@quantity = quantity
@@ -12,15 +14,18 @@ class Product
     return @quantity
   end
   
-  def setName(name)
-    @name = name
-  end
-  
   def setQuantity(quantity)
     @quantity = quantity
   end
   
   def incrementQuantity
     @quantity = @quantity + 1
+  end
+  
+  # creates a hash from this product
+  #  key is the product's name, value is the quantity
+  def hash
+    newHash = { @name => @quantity }
+    return newHash
   end
 end
