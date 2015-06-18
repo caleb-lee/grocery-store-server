b52186e44b76b1d28e25909cdd461584958d1127
Caleb Lee <caleb.lee@vokalinteractive.com>
2015-06-18 11:39:21 -0500

    Adds tests for Product

--- grocery_store_tester.rb
+++ grocery_store_tester.rb
@@ -1,2 +1,35 @@
-require 'test/unit'
+require 'minitest/autorun'
 require './product'
+
+class TestProduct < MiniTest::Test
+  def setup
+    @product = Product.new("apples", 3)
+  end
+
+  def test_initialize_and_getters
+    product = Product.new("oranges", 70)
+    assert_equal("oranges", product.name, "Initialize name doesn't work")
+    assert_equal(70, product.quantity, "Initialize quantity doesn't work")
+  end
+  
+  def test_setQuantity
+    @product.setQuantity 5
+    assert_equal(5, @product.quantity, "setQuantity doesn't work")
+  end
+  
+  def test_incrementQuantity
+    oldQuantity = @product.quantity
+    
+    @product.incrementQuantity
+    
+    assert_equal(oldQuantity + 1, @product.quantity, "incrementQuantity doesn't work")
+  end
+  
+  def test_decrementQuantity
+    oldQuantity = @product.quantity
+    
+    @product.decrementQuantity
+    
+    assert_equal(oldQuantity - 1, @product.quantity, "decrementQuantity doesn't work")
+  end
+end
