774c45e0ade28fa30247a702ed0b1c47a45f73d7
Caleb Lee <caleb.lee@vokalinteractive.com>
2015-06-18 11:09:27 -0500

    Changes the server script to rely on RequestStatuses and the InventoryController rather than containing all that functionality in the script

--- grocery_store_inventory_server.rb
+++ grocery_store_inventory_server.rb
@@ -3,13 +3,18 @@ require 'JSON'
 
 require './product'
 require './inventory'
+require './inventory_controller'
+require './request_status'
 
-# set up an inventory to do work on
+# set up an Inventory to do work on
 products = Inventory.new
 products.addProduct(Product.new("apples", 123))
 products.addProduct(Product.new("oranges", 62))
 products.addProduct(Product.new("milk", 54))
 products.addProduct(Product.new("eggs", 22))
+
+# set up an InventoryController with the products Inventory
+productsController = InventoryController.new(products)
 
 get '/api/inventory' do
   status 200
@@ -21,7 +26,6 @@ get '/api/inventory/:name' do
   
   if result == nil
     status 404
-    ""
   else
     status 200
     result
@@ -30,59 +34,56 @@ end
 
 post '/api/inventory/:name' do
   # find product with a certain name
-  product = products.productWithName(params['name'])
+  productName = params['name']
   
-  # show error if no product of that name exists
-  if product == nil
-    status 404
-    return ""
-  end
+  requestStatus = nil
   
-  # check for body
-  if request.body.read == "" # no body
-    product.incrementQuantity
-    
-    # set correct success status code
-    if product.quantity == 1
-      status 201
-    else
-      status 200
-    end
+  # get JSON body's data (if it exists)
+  jsonData = getJSONData
+  
+  if jsonData == nil
+    requestStatus = productsController.updateInventoryForProductWithName(productName, nil)
   else
-    request.body.rewind # required since we read it once already
-    data = JSON.parse request.body.read
-    newQuantity = data['quantity']
-    
-    if newQuantity < 1 # if it isn't positive
-      status 400
-      return "{\n  \"error\": \"Quantity must be positive.\"\n}"
-    else
-      # set correct status code
-      if product.quantity == 0
-        status 201
-      else
-        status 200
-      end
-      
-      product.setQuantity newQuantity
-    end
+    requestStatus = productsController.updateInventoryForProductWithName(productName, jsonData['quantity'])
   end
   
-  return products.formattedStringForProductWithName(product.name)
+  status requestStatus.statusCode
+  if requestStatus.success
+  	return products.formattedStringForProductWithName(productName)
+  else
+  	return requestStatus.errorJSON
+  end
 end
 
 delete '/api/inventory/:name' do
-  # find product with a certain name
-  product = products.productWithName(params['name'])
+  requestStatus = productsController.zeroStockForProductWithName(params['name'])
+  status requestStatus.statusCode
+end
+
+post '/api/purchase/:product' do
+  productName = params['product']
+  jsonData = getJSONData
+  requestStatus = nil
   
-  if product == nil
-    status 404
+  if jsonData == nil # no body
+    requestStatus = productsController.purchaseProductWithName(productName, 1)
   else
-    if product.quantity == 0
-      status 404
-    else
-      product.setQuantity(0)
-      status 200
-    end
+    quantityToPurchase = jsonData['quantity']
+    requestStatus = productsController.purchaseProductWithName(productName, quantityToPurchase)
   end
+  
+  status requestStatus.statusCode
+  return requestStatus.errorJSON # returns nil if successful
+end
+
+# returns a hash with the JSON data or nil
+def getJSONData
+  data = nil
+
+  if request.body.read != ""
+    request.body.rewind # required since we read it once already
+    data = JSON.parse request.body.read
+  end
+  
+  return data
 end
